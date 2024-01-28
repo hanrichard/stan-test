718321bf871108d7410a1b7015c088d0f1deb20f
richardhan <hxy.314@gmail.com>
2024-01-28 13:28:32 +1100

    add test helper

--- my-app/src/features/CategoryPage.test.tsx
+++ my-app/src/features/CategoryPage.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Category } from './CategoryPage';
-import { MemoryRouter } from "react-router-dom";
 import { customRender } from '../test/test-utils';
 
 test('renders loading...', () => {
